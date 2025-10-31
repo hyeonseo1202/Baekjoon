9b89574efb429719476b8e72ecb4a374d36893e7
hwanghyeonseo <hhseo1202@naver.com>
2025-10-31 14:57:18 +0900

    dp-bottomup

--- 2025/py/climbing_stairs.py
+++ 2025/py/climbing_stairs.py
@@ -1,7 +1,10 @@
+memo = {}
 def cs(n):
     if n == 1:
         return 1
     if n == 2:
         return 2
     
-    return cs(n-1) + cs(n-2)
+    for i in range(3, n+1):
+        memo[i] = memo[i-1]+memo[i-2]
+    return memo[n]
