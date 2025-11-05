2e30c24a756173ca310016f31cb74e2f44e666e1
hwanghyeonseo <hhseo1202@naver.com>
2025-11-05 18:30:50 +0900

    restart

--- learning/dijkstra.py
+++ learning/dijkstra.py
@@ -1,17 +1,4 @@
-import heapq
-def dijkstra(graph, start, final):
-    costs = {}
-    pq = [] # list로 구현해야함.
-    heapq.heappush(pq, (0, start))
-    
-    while pq:
-        cur_cost, cur_v = heapq.heappop(pq)
-        if cur_v not in costs:
-            costs[cur_v] = cur_cost
-            for cost, next_v in graph[cur_v]:
-                next_cost = cur_cost + cost
-                heapq.heappush(pq, (next_cost, next_v))
-    return costs[final-1]
+
 
 graph = {
     1: [(2, 2), (5, 3)],
