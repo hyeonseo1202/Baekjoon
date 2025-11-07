8bca6e1586845d3db0593a8831b78e8928cf3f5b
hwanghyeonseo <hhseo1202@naver.com>
2025-11-07 21:47:14 +0900

    1

---
+++ learning/permute.py
@@ -0,0 +1,18 @@
+def permute(nums):
+    def backtrack(curr):
+        if len(curr) == len(nums):
+            ans.append(curr[:])
+            return
+        
+        for num in nums:
+            if num not in curr:
+                curr.append(num)
+                backtrack(curr)
+                curr.pop()
+        
+    ans = []
+    backtrack([])
+    return ans
+
+nums = [1,2,3,4]
+print(permute(nums))
