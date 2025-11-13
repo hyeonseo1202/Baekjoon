707de1109366af30167d02c0a4e5237f90026873
hwanghyeonseo <hhseo1202@naver.com>
2025-11-13 14:56:53 +0900

    1

--- learning/permute.py
+++ learning/permute.py
@@ -1,5 +1,5 @@
 #Q. nums = [1,2,3,4]로 만들 수 있는 모든 순열을 반환하시오.
-def permute(nums):
+def permute(nums): #permute: 순서를 바꾸다, 뒤섞다. 과학에선 '순열'
     def backtrack(curr):
         if len(curr) == len(nums):
             ans.append(curr[:])
