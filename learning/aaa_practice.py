71a879145906f4accbdbe4671f695fad4da29d9d
hwanghyeonseo <hhseo1202@naver.com>
2025-11-24 23:56:58 +0900

    1

--- learning/aaa_practice.py
+++ learning/aaa_practice.py
@@ -1,11 +1,37 @@
+#1260: DFS와 BFS
 import sys
+from collections import deque
 input = sys.stdin.readline
 
-n = int(input())
+N,M,V = map(int, input.split())
+graph = [[]for _ in range(N+1)]
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+#정점 번호 작은 것부터 방문해야 하므로 정렬!
+for i in range(1, N+1):
+    graph[i].sort()
 
-triangle = [list(map(int, input().split())) for _ in range(n)]
 
-# DP 사용 (triangle 자체를 누적합으로 변경)
+def dfs(v,visited):
+    visited[v] = True
+    print(v, end=' ')
+    for next in graph[v]:
+        if not visited[next]:
+            dfs(next, visited)
+            
+def bfs(start):
+    visited = [False]*(N+1)
+    queue = deque([start])
+    visited[start] = True
+    
+    while queue:
+        v = queue.popleft()
+        print(v, end = ' ')
+        for next in graph[v]
 
-
-print(max(triangle[-1]))
+visited = [False]*(N+1)
+dfs(V,visited)
+print()
+bfs(V)
