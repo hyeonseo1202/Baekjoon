211254ee8d9f1410bb9c259e841d8667a02321b7
hwanghyeonseo <hhseo1202@naver.com>
2025-11-25 00:30:16 +0900

    done

--- learning/aaa_practice.py
+++ learning/aaa_practice.py
@@ -1,40 +1,33 @@
-#1260: DFS와 BFS
-import sys
+#2606 바이러스
 from collections import deque
+import sys
 input = sys.stdin.readline
 
-N,M,V = map(int, input().split())
-graph = [[]for _ in range(N+1)]
-for _ in range(M):
+computer = int(input().strip())
+edge = int(input().strip())
+graph = [[]for _ in range(computer + 1)]
+for _ in range(edge):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
-#정점 번호 작은 것부터 방문해야 하므로 정렬!
-for i in range(1, N+1):
-    graph[i].sort()
 
-
-def dfs(v,visited):
-    visited[v] = True
-    print(v, end=' ')
-    for next in graph[v]:
-        if not visited[next]:
-            dfs(next, visited)
-            
 def bfs(start):
-    visited = [False]*(N+1)
+    visited = [False]*(computer + 1)
     queue = deque([start])
     visited[start] = True
     
     while queue:
         v = queue.popleft()
-        print(v, end = ' ')
         for next in graph[v]:
             if not visited[next]:
-                visited[next] = True
                 queue.append(next)
+                visited[next] = True
+    
+    result = 0      
+    for i in range(2,computer+1):
+        if visited[i]:
+            result +=1
+    
+    print(result)
 
-visited = [False]*(N+1)
-dfs(V,visited)
-print()
-bfs(V)
+bfs(1)
