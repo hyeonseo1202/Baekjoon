1f86a07507df93664e20925024a2653f9c85b292
hwanghyeonseo <hhseo1202@naver.com>
2025-11-26 12:24:15 +0900

    done

--- learning/aaa_practice.py
+++ learning/aaa_practice.py
@@ -1,38 +1,40 @@
-#2178 미로탐색
-#행렬형태라면, 풀이는 이렇게!!! 죄표로!!!
+#2667 단지 번호 붙이기
 from collections import deque
 import sys
 input = sys.stdin.readline
 
-N,M = map(int, input().split())
+N = int(input().strip())
+squaremap = [list(map(int, input().strip()))for _ in range(N)]
 
-maze = [list(map(int, input().strip()))for _ in range(N)]
 
-dist = [[0]*M for _ in range(N)]
-
-#상하좌우
 dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
 
 def bfs(x,y):
-    q = deque()
-    q.append((x,y))
-    dist[x][y] = 1
-    
-    while q:
-        x,y = q.popleft()
-        for k in range(4):
-            nx = x + dx[k]
-            ny = y + dy[k]
+    queue = deque()
+    queue.append((x,y))
+    squaremap[x][y] = 0
+    num = 1
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
             
-            if nx == N-1 and ny == M-1:
-                print(dist[x][y]+1)
-                return
-
-            if 0 <= nx < N and 0 <= ny < M:
-                if maze[nx][ny] == 1 and dist[nx][ny] == 0:
-                    dist[nx][ny] = dist[x][y]+1
-                    q.append((nx,ny))
-                    
-    
-bfs(0,0)
+            if 0 <= nx < N and 0 <= ny < N:
+                if squaremap[nx][ny] == 1:
+                    queue.append((nx,ny))
+                    num+=1
+                    squaremap[nx][ny] = 0
+    return num
+result1 = 0
+result2 = []
+for i in range(N):
+    for j in range(N):
+        if squaremap[i][j] == 1:
+            result1+=1
+            num = bfs(i,j)
+            result2.append(num) 
+print(result1)
+for cnt in sorted(result2):
+    print(cnt)
