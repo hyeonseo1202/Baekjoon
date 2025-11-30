0a09c7378a73853cf96fbb67e61bc89f0bf54f12
hwanghyeonseo <hhseo1202@naver.com>
2025-11-30 23:15:54 +0900

    try_7576

--- learning/aaa_practice.py
+++ learning/aaa_practice.py
@@ -1,8 +1,8 @@
+#7576 토마토
 from collections import deque
 import sys
 input = sys.stdin.readline
 
-t_case = int(input())
 
 dx = [0,0,-1,1]
 dy = [-1,1,0,0]
@@ -11,33 +11,40 @@ dy = [-1,1,0,0]
 def bfs(x, y):
     queue = deque()
     queue.append((x, y))
-    ground[y][x] = 0
+    ground[y][x] = 1
+    depth = 0
     while queue:
         x, y = queue.popleft()
+        flag = 0
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             if 0 <= nx < M and 0 <= ny < N:
-                if ground[ny][nx] == 1:
-                    ground[ny][nx] = 0
+                if ground[ny][nx] == 0:
+                    ground[ny][nx] = 1
                     queue.append((nx, ny))
-
-
-for _ in range(t_case):
-    M, N, K = map(int, input().split())
-
-    ground = [[0] * M for _ in range(N)]
-
-    for _ in range(K):
-        x, y = map(int, input().split())
-        ground[y][x] = 1  # ← 좌표는 ground[y][x]
-
-    cnt = 0
-
-    for y in range(N):
-        for x in range(M):
-            if ground[y][x] == 1:
-                bfs(x, y)
-                cnt += 1
+                    flag = 1
+        if flag == 1:
+            depth += 1
     
-    print(cnt)
+    return depth
+
+def all_one(ground):
+    return all(all(x==1 for x in row)for row in ground)       
+
+
+M, N= map(int, input().split())
+
+ground = [list(map(int, input().split()))for _ in range(N)]
+if all_one(ground):
+    print(0)
+else:
+    result = 0
+    for i in range(M):
+        for j in range(N):
+            if ground[j][i] == 0:
+                result = max(bfs(i,j), result)
+    if all_one(ground):
+        print(result)
+    else:
+        print(-1)
