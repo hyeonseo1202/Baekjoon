b4a6cfe486780ee672de9bf4eb59dcde65e8ed82
hwanghyeonseo <hhseo1202@naver.com>
2025-12-01 13:25:01 +0900

    11725_done

--- learning/aaa_practice.py
+++ learning/aaa_practice.py
@@ -1,36 +1,27 @@
-#1991 트리 순회
+#11725 트리의 부모찾기
 import sys
+from collections import deque
 input = sys.stdin.readline
-N = int(input())
 
-tree = {}
-for _ in range(N):
-    root, left, right = input().split()
-    tree[root] = (left, right)
+N = int(input().strip())
 
-def preorder(node):
-    if node == '.':
-        return
-    print(node, end = '')
-    preorder(tree[node][0])
-    preorder(tree[node][1])
-
-def inorder(node):
-    if node == '.':
-        return
-    inorder(tree[node][0])
-    print(node, end = '')
-    inorder(tree[node][1])
+graph = [[]for _ in range(N+1)]
+for _ in range(N-1):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
     
-def postorder(node):
-    if node == '.':
-        return
-    postorder(tree[node][0])
-    postorder(tree[node][1])
-    print(node, end = '')
+parent = [0] * (N+1)
+parent[1] = 0
 
-preorder('A')
-print()
-inorder('A')
-print()
-postorder('A')
+q = deque([1])
+
+while q:
+    cur = q.popleft()
+    for nxt in graph[cur]:
+        if parent[nxt] == 0:
+            parent[nxt] = cur
+            q.append(nxt)
+
+for i in range(2,N+1):
+    print(parent[i])
