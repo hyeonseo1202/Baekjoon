c2254b9cc3025b225ac0986a458f3ddf91592578
hwanghyeonseo <121742251+hyeonseo1202@users.noreply.github.com>
2025-12-11 20:27:38 +0900

    [Gold IV] Title: 이분 그래프, Time: 1076 ms, Memory: 55208 KB -BaekjoonHub

---
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -0,0 +1,41 @@
+# 1707 이분그래프 DFS
+import sys
+sys.setrecursionlimit(10**7)
+input = sys.stdin.readline
+
+K = int(input().strip())
+answers = []
+
+for _ in range(K):
+    V, E = map(int, input().split())
+    graph = [[] for _ in range(V + 1)]
+
+    for _ in range(E):
+        a, b = map(int, input().split())
+        graph[a].append(b)
+        graph[b].append(a)
+
+    color = [0] * (V + 1)   # 0: 미방문, 1 / -1: 두 색
+
+    def dfs(cur, c):
+        color[cur] = c
+        for nxt in graph[cur]:
+            if color[nxt] == 0:              # 아직 방문 X
+                if not dfs(nxt, -c):         # 아래에서 False 나오면 그대로 전파
+                    return False
+            elif color[nxt] == c:            # 인접 정점이 같은 색이면 이분 그래프 아님
+                return False
+        return True
+
+    is_bipartite = True
+
+    # 여러 컴포넌트가 있을 수 있으므로 1 ~ V 모두 돌기
+    for v in range(1, V + 1):
+        if color[v] == 0:
+            if not dfs(v, 1):
+                is_bipartite = False
+                break
+
+    answers.append("YES" if is_bipartite else "NO")
+
+print("\n".join(answers))
