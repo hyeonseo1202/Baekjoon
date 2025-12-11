5d7df6bed39f9d3e27ddd16a5404e190190f7dba
hwanghyeonseo <hhseo1202@naver.com>
2025-12-11 20:13:46 +0900

    1707_DFS_Done

--- learning/aaa_practice.py
+++ learning/aaa_practice.py
@@ -1,34 +1,42 @@
-#2644 촌수계산
+#1707 이분그래프 DFS
+
+
+# 이분그래프란?
+# - 정점을 두 그룹(집합)으로 나눌 수 있고, 
+# - 모든 간선은 항상 서로 다른 그룹끼리만 연결되는 그래프
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**7)
 
-n = int(input().strip())
-a,b = map(int, input().split())
-m = int(input().strip())
-
-graph = [[]for _ in range(n + 1)]
-
-for _ in range(m):
-    x,y = map(int, input().split())
-    graph[x].append(y)
-    graph[y].append(x)
-
-visited = [False]*(n+1)
-answer = -1
-
-def dfs(cur,depth):
-    global answer
-    if cur == b:
-        answer = depth
-        return
-
-    visited[cur] = True
+K = int(input().strip())
+for _ in range(K):
+    V, E = map(int, input().split())
+    graph = [] *(V+1)
     
-    for nxt in graph[cur]:
-        if not visited[nxt]:
-            dfs(nxt, depth+1)
-
-dfs(a,0)
-print(answer)
+    for _ in range(E):
+        a,b = map(int,input().split())
+        graph[a].append(b)
+        graph[b].append(a)
         
+    color = [0] * (V+1)
+    flag = True
+    
+    def dfs(cur, c):
+        global flag
+        if flag == False:
+            return
+        
+        color[cur] = c
+        for next in graph[cur]:
+            if color[next] == 0:
+                dfs(next, -c)
+            elif color[next] == c:
+                flag = False
+                return
+        
+    for v in range(1, V+1):
+        if color[v] == 0:
+            dfs(v,1)
+            if flag == False:
+                break
+    
+    print("YES" if flag else "NO")
