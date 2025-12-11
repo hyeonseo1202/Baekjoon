1d222a422f2ba84e6b048a35c3bc3b3ad7da0c11
hwanghyeonseo <hhseo1202@naver.com>
2025-12-11 19:37:36 +0900

    2644_dfs_done

--- learning/aaa_practice.py
+++ learning/aaa_practice.py
@@ -1,53 +1,34 @@
-#16234 인구이동
-from collections import deque
+#2644 촌수계산
 import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**7)
 
-N,L,R = map(int, input().split())
-graph = [list(map(int, input().split()))for _ in range(N)]
+n = int(input().strip())
+a,b = map(int, input().split())
+m = int(input().strip())
 
-dx = [0,0,-1,1]
-dy = [-1,1,0,0]
+graph = [[]for _ in range(n + 1)]
 
-result = 0
-while True:
+for _ in range(m):
+    x,y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+
+visited = [False]*(n+1)
+answer = -1
+
+def dfs(cur,depth):
+    global answer
+    if cur == b:
+        answer = depth
+        return
+
+    visited[cur] = True
     
-    visited = [[False]*N for _ in range(N)]
-    abq = deque()
-    def bfs(x,y):
-        q = deque()
-        q.append((x,y))
-        abq.append((x,y))
-        visited[x][y] = True
-        total_num = 1
-        total_sum = graph[x][y]
-        while q:
-            x, y = q.popleft()
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-                if 0<=nx<N and 0<=ny<N:
-                    if visited[nx][ny]==False and  L<= abs(graph[nx][ny] - graph[x][y]) <= R:
-                        visited[nx][ny] = True
-                        q.append((nx,ny))
-                        abq.append((nx,ny))
-                        total_num+=1
-                        total_sum+=graph[nx][ny]
-        return total_num, total_sum
-    
-    # 이 for문이 한 번 완료되면, 1일 지난 것
-    flag = 0
-    for x in range(N):
-        for y in range(N):
-            if visited[x][y] == False:
-                abq.clear()
-                a,b = bfs(x,y)
-                if a>=2:
-                    flag = 1
-                    while abq:
-                        i,j = abq.popleft()
-                        graph[i][j] = b//a
-    if flag == 1:
-        result +=1
-    else:
-        print(result)
-        break
+    for nxt in graph[cur]:
+        if not visited[nxt]:
+            dfs(nxt, depth+1)
+
+dfs(a,0)
+print(answer)
+        
