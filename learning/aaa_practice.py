2795d07bb8488182cf09c442a4d2b1904ed93313
hwanghyeonseo <hhseo1202@naver.com>
2025-12-17 13:58:03 +0900

    1463_done_top_down

--- learning/aaa_practice.py
+++ learning/aaa_practice.py
@@ -3,29 +3,30 @@
 #시간초과는 메모이제이션 사용하지 않았기 때문에 발생함!!
 # dp배열을 써서 메모이제이션 사용하면, 시간초과 나지 않도록 할 수 있음
 import sys
+sys.setrecursionlimit(10**7)
 input = sys.stdin.readline
 
 N = int(input())
 
-
+dp = [-1]*(N+1)
 
 def top_down(n):
     
     if n == 1:
         return 0
-    elif n == 2:
-        return 1
-    elif n == 3:
-        return 1
     
-    if n%3 == 0 and n%2 == 0:
-        return min(top_down(n//3), top_down(n//2), top_down(n-1))+1
-    elif n%3 == 0:
-        return min(top_down(n//3), top_down(n-1))+1
-    elif n%2 == 0:
-        return min(top_down(n//2), top_down(n-1))+1
-    else:
-        return top_down(n-1)+1
+    if dp[n] != -1:
+        return dp[n]
+    
+    res = top_down(n-1)+1
+    
+    if n%2 == 0:
+        res = min(res, top_down(n//2)+1)
+    
+    if n%3 == 0:
+        res = min(res, top_down(n//3)+1)
+    
+    dp[n] = res
+    return res
 
-ans = top_down(N)
-print(ans)
+print(top_down(N))
