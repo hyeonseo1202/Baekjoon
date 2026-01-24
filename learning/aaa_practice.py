21f2ab8e98f03e09850b8ed8118864342a6e564b
hwanghyeonseo <hhseo1202@naver.com>
2026-01-24 20:31:44 +0900

    17412_done

--- learning/aaa_practice.py
+++ learning/aaa_practice.py
@@ -1,35 +1,64 @@
-#1753 최단경로
-
+#17412 연구소3
+from collections import deque
+from itertools import combinations
 import sys
-import heapq
-input = sys.stdin.readline
+imput = sys.stdin.readline
 
-INF = 10**18
+N,M = map(int, input().split())
+lab = [list(map(int, input().split()))for _ in range(N)]
 
-V, E = map(int, input().split())
-K = int(input())
+dx = [0,0,-1,1]
+dy = [-1,1,0,0]
 
-graph = [[]for _ in range(V+1)]
-for _ in range(E):
-    u, v, w = map(int, input().split())
-    graph[u].append((v, w))
+#기본 바이러스들 집합
+viruses = []
+for y in range(N):
+        for x in range(N):
+            if lab[y][x] == 2:
+                viruses.append((x,y))
+ 
+#활성 바이러스들 받아서 한 번에 시작            
+def spread(board, active_viruses):
+    fin_depth = 0
+    q = deque()
+    for x, y in active_viruses:
+        q.append((x, y, 0))
+        
+    while q:
+        x, y, depth = q.popleft()
+        for k in range(4):
+            nx,ny = x + dx[k], y+dy[k]
+            if 0 <= nx < N and 0 <= ny < N:
+                # 빈칸(0)일 때만 시간이 의미 있음
+                if board[ny][nx] == 0:
+                    board[ny][nx] = -1
+                    fin_depth = max(fin_depth, depth + 1)
+                    q.append((nx, ny, depth + 1))
 
-dist = [INF]*(V+1)
-dist[K] = 0
-pq = [(0,K)]
+                # 비활성 바이러스(2)는 "통과"만 (fin_depth 갱신 X)
+                elif board[ny][nx] == 2:
+                    board[ny][nx] = -1
+                    q.append((nx, ny, depth + 1))
+    for y in range(N):
+        for x in range(N):
+            if board[y][x] == 0:
+                return float('inf')  # 실패
+    return fin_depth
 
-while pq:
-    cur_dist, cur = heapq.heappop(pq)
-    if cur_dist != dist[cur]:
-        continue
+
+
+ans = float('inf')
+
+for walls in combinations(viruses, M):
+    board = [row[:] for row in lab]
+    active_viruses = []
+    for x, y in walls:
+        board[y][x] = -1
+        active_viruses.append((x,y))
     
-    for nxt, w in graph[cur]:
-        nd = cur_dist + w
-        if nd < dist[nxt]:
-            dist[nxt] = nd
-            heapq.heappush(pq, (nd, nxt))
-                
-                
-
-
-
+    ans = min(ans, spread(board, active_viruses))
+    
+if ans == float('inf'):
+    print(-1)
+else:
+    print(ans)
